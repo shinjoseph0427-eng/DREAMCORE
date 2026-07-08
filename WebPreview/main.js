88a6b65816ca2ccfcbdf8c8c7ede6102f49b68e5
shinjoseph0427 <jiseongsin68@gmail.com>
2026-07-08 05:20:12 -0700

    Increase mobile swipe look sensitivity

--- WebPreview/main.js
+++ WebPreview/main.js
@@ -1472,7 +1472,7 @@ const mobileLook = {
   pitch: 0,
 };
 const mobileLookEuler = new THREE.Euler(0, 0, 0, 'YXZ');
-const MOBILE_LOOK_SENSITIVITY = 0.0026;
+const MOBILE_LOOK_SENSITIVITY = 0.0030;
 const MOBILE_TAP_MOVE_SECONDS = 0.34;
 const MOBILE_LONG_PRESS_MS = 260;
 const MOBILE_DRAG_DEADZONE = 7;
